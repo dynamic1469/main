fcad41a05ac60fbcd3f3fc1806978757cbf5179c
khalnayak14 <ricrawat4321@gmail.com>
2023-11-21 00:09:42 +0530

    scroll
    ada

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ var typed = new Typed(".multiple-text", {
     loop: true
 
 })
+let sections = document.querySelectorAl1('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        if (top >= offset && top < offset + height) {
+            sec.classList.add(' show-animate');
+        }
+        else {
+            sec.classList.remove('show-animate');
+        }
+    })
+}
