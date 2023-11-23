a667551b60cf58a708c8ba76642fe1344f742066
khalnayak14 <ricrawat4321@gmail.com>
2023-11-23 17:48:06 +0530

    skadad
    asdada

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
-var typed = new typed(".multiple-text", {
-    strings: ["OASIS", " ALT F4 ", "GRAPHIC ERA", "AMAZON", "GOOGLE"],
-    typespeed: 100,
-    backspeed: 100,
-    backDelay: 1000,
-    loop: true
+let wrapper = document.querySelector('#wrapper');
+wrapper.dataset.mousedownat = 0;
+wrapper.dataset.previous_movd = 0;
+
+wrapper.addEventListener("mousedown", function (loc_md) {
+
+    wrapper.dataset.mousedownat = loc_md.clientX;
+
+});
+
+wrapper.addEventListener("mousemove", function (loc) {
+
+    if (parseFloat(wrapper.dataset.mousedownat) === 0)
+        return;
+
+
+
+    let mousemoved = ((((loc.clientX - parseFloat(wrapper.dataset.mousedownat)) / window.innerWidth) * -200) + parseFloat(wrapper.dataset.previous_movd));
+    wrapper.dataset.previous = mousemoved;
+    var card = document.querySelectorAll(".card");
+
+    card.forEach(function (c) {
+
+        c.animate({
+            objectPosition: `${100 + mousemoved}% 50%`
+        }, { duration: 1200, fill: "forwards" });
+    })
+
+    var cards = document.querySelector(".cards");
+
+
+    cards.animate({
+        transform: `translate(${mousemoved}%,0)`
+    }, { duration: 1200, fill: "forwards" });
+
+});
+
+wrapper.addEventListener("mouseup", function () {
+
+    wrapper.dataset.mousedownat = 0;
+    wrapper.dataset.previous_movd = parseFloat(wrapper.dataset.previous);
 
 })
 
-let sections = document.querySelectorAl1('section');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        if (top >= offset && top < offset + height) {
-            sec.classList.add(' show-animate');
-        }
-        else {
-            sec.classList.remove('show-animate');
-        }
+VanillaTilt.init(document.querySelector("#img-pg2"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1
+});
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
     })
-}
+});
